b3dfcd0a52d0c61860c859250b3ad85b7958ce63
Azucena27 <61021132+Azucena27@users.noreply.github.com>
2021-02-02 01:43:43 -0600

    Ejercicio 5

---
+++ GasPrices.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.ejercicio3;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Azucena
+ */
+public class GasPrices {
+    
+    public static void RangoPrecios(int pricePerBarrel){
+        double precioMenor;
+        double precioMayor;
+        
+        precioMenor=(3.5*pricePerBarrel)/100;
+        precioMayor=(4.0*pricePerBarrel)/100;
+        System.out.println("El precio del surtidor está entre $"+precioMenor+" y $"+precioMayor);
+        
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingresa el precio por barril: ");
+        int pricePerBarrel = scanner.nextInt();
+        
+        RangoPrecios(pricePerBarrel);
+        
+    }
+
+}
