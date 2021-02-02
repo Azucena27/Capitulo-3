b2b15fa0f5990dd4f47ea48786d9e4b25db04aa4
Azucena27 <61021132+Azucena27@users.noreply.github.com>
2021-02-02 01:26:43 -0600

    Ejercicio1

---
+++ Programa1.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.ejercicio3;
+
+/**
+ *
+ * @author Azucena
+ */
+public class Programa1 {
+
+    static int v = 4;
+    static int w = 6;
+    static double x = 2.2;
+
+    public static void calculate(int x, double y) {
+
+    }
+
+    public static void main(String[] args) {
+        calculate(v, w);
+        calculate(v, x);
+//        calculate(x, y);
+        calculate(18, x);
+      //  calculate(1.1, 2.2);
+        calculate(5, 7);
+
+    }
+}
