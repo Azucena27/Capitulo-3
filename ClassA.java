0717f11195e29860df031a52228cfdbaa302d805
Azucena27 <61021132+Azucena27@users.noreply.github.com>
2021-02-02 01:27:46 -0600

    Ejercicio 2

---
+++ ClassA.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.ejercicios;
+
+/**
+ *
+ * @author Azucena
+ */
+public class ClassA {
+    
+    private Integer b;
+    public Integer c;
+    public static Integer d ;
+    
+    
+}
+
+
---
+++ ClassB.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.ejercicios;
+
+import com.mycompany.ejercicios.ClassA;
+
+/**
+ *
+ * @author Azucena
+ */
+public class ClassB {
+
+    
+
+    public static void main(String[] args) {
+        
+        ClassA obA = new ClassA();
+        
+ //       obA.b = 12;
+        obA.c = 5;
+        obA.d = 23;
+  //     ClassA.b = 4;
+   //     ClassA.c = 33;
+        ClassA.d = 99;
+    }
+
+}
