73e161a49c349ed688a1beb212defc8570c8d986
Azucena27 <61021132+Azucena27@users.noreply.github.com>
2021-02-04 01:42:22 -0600

    Ejercicio 13

---
+++ BankAccount.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.ejercicio3;
+
+/**
+ *
+ * @author Azucena
+ */
+public class BankAccount {
+
+    public int numCuenta;
+    public String nombre;
+    public double saldo;
+
+    BankAccount() {
+        numCuenta = 0;
+        nombre = "";
+        saldo = 0;
+    }
+
+    public void deductMonthlyFee(double saldo) {
+        double saldoFinal;
+        saldoFinal = saldo - 4;
+        System.out.println("El saldo actual es: $" + saldoFinal);
+    }
+
+    public void explainAccountPolicy() {
+        System.out.println("La tarifa de servicio es de $4, la cual se descontará cada mes al saldo de la cuenta del propetario. ");
+    }
+
+    /**
+     * @return the numCuenta
+     */
+    public int getNumCuenta() {
+        return numCuenta;
+    }
+
+    /**
+     * @param numCuenta the numCuenta to set
+     */
+    public void setNumCuenta(int numCuenta) {
+        this.numCuenta = numCuenta;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the saldo
+     */
+    public double getSaldo() {
+        return saldo;
+    }
+
+    /**
+     * @param saldo the saldo to set
+     */
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+}
---
+++ TestBankAccount.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.ejercicio3;
+
+import com.mycompany.ejercicio3.BankAccount;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Azucena
+ */
+public class TestBankAccount {
+
+    public static void main(String[] args) {
+
+        BankAccount cuenta1 = new BankAccount();
+        BankAccount cuenta2 = new BankAccount();
+        BankAccount cuenta3 = new BankAccount();
+        BankAccount cuenta4 = new BankAccount();
+        cuenta1 = getData();
+        cuenta2 = getData();
+        cuenta3 = getData();
+        showValues(cuenta1);
+        showValues(cuenta2);
+        showValues(cuenta3);
+        showValues(cuenta4);
+
+    }
+
+    public static BankAccount getData() {
+        Scanner scanner = new Scanner(System.in);
+        String nombreUsuario;
+        BankAccount cuentaBancaria = new BankAccount();
+        System.out.println("Ingrese el número de cuenta: ");
+        cuentaBancaria.setNumCuenta(scanner.nextInt());
+        System.out.println("Ingrese el nombre del propietario de la cuenta: ");
+        //cuentaBancaria.setNombre(scanner.nextLine());
+        nombreUsuario = scanner.nextLine();
+        cuentaBancaria.setNombre(nombreUsuario);
+        System.out.println("Ingrese el saldo de cuenta: ");
+        cuentaBancaria.setSaldo(scanner.nextDouble());
+        return cuentaBancaria;
+    }
+
+    public static void showValues(BankAccount cuenta) {
+        System.out.println("El número de cuenta es: " + cuenta.getNumCuenta());
+        System.out.println("El nombre del propetario de la cuenta es: " + cuenta.getNombre());
+        System.out.println("El saldo de la cuenta es: $" + cuenta.getSaldo());
+        cuenta.deductMonthlyFee(cuenta.getSaldo());
+        cuenta.explainAccountPolicy();
+    }
+}
